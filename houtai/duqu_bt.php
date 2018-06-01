8b3dbf2d4cbf74e9a7fa1c28c490260e65a72248
qq1327300698 <1327300698@qq.com>
2018-06-01 19:25:30 +0800

    “1”

---
+++ houtai/duqu_bt.php
@@ -0,0 +1,26 @@
+<?php
+    $servername = "localhost";
+$username = "root";
+$password = "sunhaiwei1998";
+$dbname = "shiping_db";
+$q=$_GET["q"];
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+ // 检测连接
+if ($conn->connect_error) {
+     die("连接失败: " . $conn->connect_error);
+}
+mysqli_set_charset($conn,"utf8");
+$sql = "SELECT * FROM shiping_tb WHERE jmBt= '".$q."'" ;
+$result=mysqli_query($conn, $sql);
+if(mysqli_num_rows($result)>0){
+    while($row=mysqli_fetch_array($result)){
+        echo $row["id"].",".$row["jmBt"].",".$row["jmMl"].",".$row["jmTp"].",".$row["jmJj"].",".$row["jmBq"].",".$row["spBt"].",".$row["spLj"].",".$row["spJj"].",".$row["sj"].",".$row["spLb"];
+
+    }
+}
+else{
+    echo "0 结果";
+}
+mysqli_close($conn);
+?>
